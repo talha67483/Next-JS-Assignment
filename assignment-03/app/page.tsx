8c06975d86c37502c082205ecfc02aebde029a42
talha67483 <mh2971966@gmail.com>
2024-10-17 21:33:11 -0700

    add file

---
+++ assignment-03/app/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+
+
+<Hero/>
+
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default page
